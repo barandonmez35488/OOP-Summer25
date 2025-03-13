b4f1dc8a7e7968c9e3423f73f68131bbfde4c8b2
barandonmez35488 <baran.donmez@akademiata.edu.pl>
2025-03-13 11:06:49 +0100

    Create student_details_structure.py

---
+++ student_details_structure.py
@@ -0,0 +1,12 @@
+
+student1 = 
+    'first_name': 'Baran',
+    'last_name': 'Donmez',
+    'age': 21,
+    'email': 'baran.donmez@akademiata.edu.pl'
+
+
+(f"First Name: {student1['baran']}")
+(f"Last Name: {student1['donmez']}")
+(f"Age: {student1['21']}")
+(f"Email: {student1['baran.donmez@akademiata.edu.pl']}")
