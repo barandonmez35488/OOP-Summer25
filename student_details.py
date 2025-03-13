72b22c35c771dd0da04042f0f1a9bd9f87b7372e
barandonmez35488 <baran.donmez@akademiata.edu.pl>
2025-03-13 10:38:44 +0100

    Create student_details.py

---
+++ student_details.py
@@ -0,0 +1,14 @@
+my_age = 21
+ my_indexnumber = 35488
+ my_name = "baran donmez "
+ my_surname = "begen"
+ nationality = "turja"
+ starting_date = "01.01.2025"
+courses = ["Mathematics", "Physics", "Computer Science"]
+ print(my_age,
+ my_indexnumber,
+ my_name ,
+ my_surname,
+ nationality,
+ starting_date,
+ courses)
