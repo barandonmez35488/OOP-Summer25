56100f2fa438387a87d99a534b96ea52a2e7e5ac
barandonmez35488 <baran.donmez@akademiata.edu.pl>
2025-03-20 10:36:54 +0100

    animals.py

---
+++ animals.py
@@ -0,0 +1,38 @@
+axolotl = {
+    "name": "Axolotl",
+    "group": "Amphibians",
+    "number_of_legs": 4,
+    "skills": ["regenerating limbs", "swimming", "breathing through skin"]
+}
+
+quokka = {
+    "name": "Quokka",
+    "group": "Mammals",
+    "number_of_legs": 4,
+    "skills": ["smiling", "jumping", "foraging"]
+}
+
+kakapo = {
+    "name": "Kakapo",
+    "group": "Birds",
+    "number_of_legs": 2,
+    "skills": ["climbing", "parrot-like mimicry", "nocturnal behavior"]
+}
+
+dung_beetle = {
+    "name": "Dung Beetle",
+    "group": "Insects",
+    "number_of_legs": 6,
+    "skills": ["rolling dung", "navigating using stars", "burrowing"]
+}
+
+saola = {
+    "name": "Saola",
+    "group": "Mammals",
+    "number_of_legs": 4,
+    "skills": ["camouflage", "speed", "elusive behavior"]
+}
+
+animals = [axolotl, quokka, kakapo, dung_beetle, saola]
+
+
